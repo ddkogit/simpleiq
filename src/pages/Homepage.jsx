bb1e39bdf619bdb1b0c0479cb6e7990f1fe12809
ddkogit <git config --global user.email>
2024-07-07 18:08:38 +0545

    home page created

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import "./Homepage.css"
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <main className='homemain'>
+      <h1 className='homeheading'>
+        Test Your IQ With Simple IQ for Free
+      </h1>
+    <section className='rules'>
+    <ul>
+      <li>There are 20 questions in random difficulty.</li>
+      <li>Select one right answer out of four options.</li>
+      <li>Submit and get your IQ Score for free.</li>
+    </ul>
+    </section>
+    <Link  to="/testpage">
+    <Button text="Start Test" type="primary"  />
+    </Link>
+    </main> 
   )
 }
 
